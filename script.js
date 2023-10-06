99b40c83610fb307370f7a121cf262ac5957fc5b
Shawn Arlantico <arlantico.shawn@gmail.com>
2023-10-06 00:12:42 -0700

    modified the js code that follows the order of operations to allow the calculator to calculate expressions like 2+4/4*3-1 correctly

--- script.js
+++ script.js
@@ -1,52 +1,61 @@
 let displayValue = '';
 
 function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById('display').value = displayValue;
+   displayValue += value;
+   document.getElementById('display').value = displayValue;
 }
 
 function clearDisplay() {
-    displayValue = '';
-    document.getElementById('display').value = '';
+   displayValue = '';
+   document.getElementById('display').value = '';
 }
 
 function calculate() {
-    try {
-        // Use regular expression to split the input into operands and operators
-        const expression = displayValue;
-        const operators = expression.split(/[0-9.]+/).filter(Boolean);
-        const operands = expression.split(/[-+*/]+/).map(Number);
+   try {
+      // Use regular expression to split the input into operands and operators
+      const expression = displayValue;
+      const operators = expression.split(/[0-9.]+/).filter(Boolean);
+      const operands = expression.split(/[-+*/]+/).map(Number);
 
-        if (operators.length === operands.length - 1) {
-            let result = operands[0];
+      if (operators.length === operands.length - 1) {
+         // Calculate expressions following the order of operations
+         const orderOfOperations = [['*', '/'], ['+', '-']];
+         for (const operatorsGroup of orderOfOperations) {
             for (let i = 0; i < operators.length; i++) {
-                switch (operators[i]) {
-                    case '+':
-                        result += operands[i + 1];
+               if (operatorsGroup.includes(operators[i])) {
+                  switch (operators[i]) {
+                     case '*':
+                        operands[i] *= operands[i + 1];
                         break;
-                    case '-':
-                        result -= operands[i + 1];
-                        break;
-                    case '*':
-                        result *= operands[i + 1];
-                        break;
-                    case '/':
+                     case '/':
                         if (operands[i + 1] === 0) {
-                            throw new Error('Division by zero');
+                           throw new Error('Division by zero');
                         }
-                        result /= operands[i + 1];
+                        operands[i] /= operands[i + 1];
                         break;
-                    default:
+                     case '+':
+                        operands[i] += operands[i + 1];
+                        break;
+                     case '-':
+                        operands[i] -= operands[i + 1];
+                        break;
+                     default:
                         throw new Error('Invalid operator');
-                }
+                  }
+                  operators.splice(i, 1);
+                  operands.splice(i + 1, 1);
+                  i--;
+               }
             }
-            displayValue = result.toString();
-            document.getElementById('display').value = result;
-        } else {
-            throw new Error('Invalid expression');
-        }
-    } catch (error) {
-        displayValue = '';
-        document.getElementById('display').value = 'Error';
-    }
+         }
+         const result = operands[0];
+         displayValue = result.toString();
+         document.getElementById('display').value = result;
+      } else {
+         throw new Error('Invalid expression');
+      }
+   } catch (error) {
+      displayValue = '';
+      document.getElementById('display').value = 'Error';
+   }
 }

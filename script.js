6d09f31d8698ed9de7104cf66f5a917837e489f7
Shawn Arlantico <arlantico.shawn@gmail.com>
2023-10-05 23:36:47 -0700

    Using regular expressions, the input is divided into operands and operators. The operands are numerically transformed. Iterating over the operators, the code performs the relevant mathematical operation step by step. Error handling for division by zero and incorrect expressions has been introduced.

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+let displayValue = '';
 
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        // Use regular expression to split the input into operands and operators
+        const expression = displayValue;
+        const operators = expression.split(/[0-9.]+/).filter(Boolean);
+        const operands = expression.split(/[-+*/]+/).map(Number);
+
+        if (operators.length === operands.length - 1) {
+            let result = operands[0];
+            for (let i = 0; i < operators.length; i++) {
+                switch (operators[i]) {
+                    case '+':
+                        result += operands[i + 1];
+                        break;
+                    case '-':
+                        result -= operands[i + 1];
+                        break;
+                    case '*':
+                        result *= operands[i + 1];
+                        break;
+                    case '/':
+                        if (operands[i + 1] === 0) {
+                            throw new Error('Division by zero');
+                        }
+                        result /= operands[i + 1];
+                        break;
+                    default:
+                        throw new Error('Invalid operator');
+                }
+            }
+            displayValue = result.toString();
+            document.getElementById('display').value = result;
+        } else {
+            throw new Error('Invalid expression');
+        }
+    } catch (error) {
+        displayValue = '';
+        document.getElementById('display').value = 'Error';
+    }
+}
